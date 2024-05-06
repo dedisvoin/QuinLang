952e259e08fa4ec0716b82690436bf8a8c2feb91
dedisvoin <88434293+dedisvoin@users.noreply.github.com>
2024-05-06 22:02:18 +0300

    Delete src directory

--- src/ml_tokenizer/keywords.py
+++
@@ -1,6 +0,0 @@
-
-keywords = ['fn', 'for', 'if',' else', 'while', 'let', 'const', 'and','or', 'not','auto','forin', 'match', 'case']
-
-keywords_types = ['int', 'float', 'str', 'void', 'list']
-
-keywords_functs = ['print', 'input', 'range']
